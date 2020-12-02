136f534cd902ab9d98ee6b37b222d2ecdf0aefb6
Christos Mantas <mantas.chris@gmail.com>
2020-12-02 22:26:07 +0200

    Make proper teacher forcing and prediction for EDModel and children

--- encoder_decoder_model.py
+++ encoder_decoder_model.py
@@ -3,8 +3,15 @@ from spelling_model import SpellingModel
 
 class EDModel(S2SModel):
     BATCH_SIZE = 100 # smaller batches seem to have fastest conversion
+    CHAR_CODE_START = '\t'
 
-    def create_model(self, latent_dim=128):
+    def init_from_texts(self, texts):
+         super().init_from_texts(texts + [self.CHAR_CODE_START])
+
+    def start_token(self):
+        return self.tokenizer.word_index[self.CHAR_CODE_START]
+
+    def create_model(self):
         # Inputs
         encoder_input = Input(shape=(self.max_seq_length), dtype='int32')
         decoder_input = Input(shape=(self.max_seq_length), dtype='int32')
@@ -34,37 +41,54 @@ class EDModel(S2SModel):
         self.model = Model(inputs=[encoder_input, decoder_input], outputs=[output])
         self.compile_model()
 
+    def vectorize_output_batch(self, texts):
+        padded = ["\t" + t for t in texts]
+        return self.vectorize_batch(padded)
+
     def vectorize_pairs(self, in_texts, out_texts):
         encoder_input = self.vectorize_batch(in_texts)
 
+        decoder_output = self.vectorize_output_batch(out_texts)
+
         # TODO: maybe the encoder input is not like encoder input
         decoder_input = np.zeros_like(encoder_input)
-        decoder_input[:, 1:] = encoder_input[:, :-1]
-        #decoder_input[:, 0] = self.tokenizer.word_index['\t']
+
+        # teacher forcing: use as decoder inputs the ground-truth outputs,
+        # shifted by +1.
+        decoder_input[:, 1:] = decoder_output[:, :-1]
+        decoder_input[:, 0] = self.start_token()
 
         X = (encoder_input, decoder_input)
         if out_texts is None:
             return X, None
 
-        decoder_output = self.vectorize_output_batch(out_texts)
+        #decoder_output = self.vectorize_output_batch(out_texts)
         Y = decoder_output
         return(X, Y)
 
+    def seq_to_text(self, seq):
+        return super().seq_to_text(seq)[1:]
 
-    def predict(self, texts):
-        (encoder_input, decoder_input), _ = self.vectorize_pairs(texts, None)
-        preds = self.model.\
-            predict([encoder_input, decoder_input]). \
-            argmax(axis=2)
+    def predict(self, in_texts):
+        encoder_input = self.vectorize_batch(in_texts)
+        decoder_input = np.zeros((len(in_texts), self.max_seq_length))
 
-        return [self.seq_to_text(seq) for seq in preds]
+        # start with the decoder input sequences all containing the start token
+        decoder_input[:, 0] = self.start_token()
 
-        # TODO: incorporate this ...
+        # each char of the output needs to be predicted based on the previous chars
         for i in range(1, self.max_seq_length):
-            output = self.model.predict([encoder_input, decoder_input]).argmax(axis=2)
+            # for each
+            output =  self.model.predict([encoder_input, decoder_input]).\
+                                 argmax(axis=2)
             decoder_input[:, i] = output[:, i]
+            # Note: this is practically identical to
+            # output = decoder_input = self.model.predict([encoder_input, decoder_input])
+            # but that doesn't read as well....
 
-        return [self.seq_to_text(seq) for seq in decoder_input]
+        decoder_output = decoder_input # why not just use `output` ?
+
+        return [self.seq_to_text(seq) for seq in decoder_output]
 
 
 class EDSpellModel(EDModel, SpellingModel):
